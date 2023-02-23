1e90433d9aee1d3af7b6a86c17efb62e778dd32c
Crgkrm <92501302+Crgkrm@users.noreply.github.com>
2023-02-23 08:26:47 -0800

    first commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from './../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+
+export class TodosComponent implements OnInit {
+todos:Todo[];
+inputTodo:string="";
+   
+constructor() { }
+  
+  
+  
+  
+  ngOnInit(): void {
+
+  this.todos=[
+    {
+    
+    content: 'first-Todo',
+    completed: false
+    },
+    {  
+      content: 'second-Todo',
+      completed: true
+      },
+      {  
+        content: 'third-Todo',
+        completed: false
+        },
+
+  ]
+
+ 
+
+
+  }
+
+  toggleDone(id:Number):void{
+   this.todos.map((v,i)=>{
+    
+    if(i==id)v.completed =!v.completed;
+
+
+     return v;
+   })
+
+  }
+
+deleteTodo(id:Number){
+  this.todos=this.todos.filter((v,i)=>i !==id);
+
+
+}
+
+
+addTodo(){
+this.todos.push({
+   content:this.inputTodo,
+   completed:true
+
+})
+
+
+
+
+}
+
+}
+
+ 
+
+
+
+
